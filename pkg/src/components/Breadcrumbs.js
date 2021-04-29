948ea2ef369dab933a02fd538e57a2aacaa5ab97
Younus <younus.abdalla@whitespace.se>
2021-04-29 14:37:14 +0200

    refactor: update breadcrumbs

--- pkg/src/components/Breadcrumbs.js
+++ pkg/src/components/Breadcrumbs.js
@@ -1,16 +1,31 @@
-// Ref: https://codepen.io/peterantonius/pen/RwKdXvr
-
 import clsx from "clsx";
 import React from "react";
+import PropTypes from "prop-types";
 
 import Link from "./Link";
 
-import * as styles from "./Breadcrumbs.module.css";
+import * as defaultStyles from "./Breadcrumbs.module.css";
+
+Breadcrumbs.propTypes = {
+  label: PropTypes.string,
+  description: PropTypes.string,
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      label: PropTypes.string.isRequired,
+      url: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+  separator: PropTypes.node,
+  styles: PropTypes.objectOf(PropTypes.string),
+  className: PropTypes.string,
+};
 
 export default function Breadcrumbs({
   label = "Brödsmulor",
   description = "Du är här:",
   items,
+  separator = "/",
+  styles = defaultStyles,
   className,
   ...restProps
 }) {
@@ -24,28 +39,27 @@ export default function Breadcrumbs({
       )}
       {...restProps}
     >
-      <div className={clsx("")}>
-        <p className={styles.description}>{description}</p>
-        <ol className={clsx(styles.list)}>
-          {items.map((item, index) => {
-            if (!item) {
-              console.warn("One breadcrumb was null", items);
-              return null;
-            }
-            return (
-              <li key={index} className={clsx(styles.item)}>
-                <Link
-                  to={item.url}
-                  className={clsx(styles.link)}
-                  aria-current={index === items.length - 1 ? "page" : null}
-                >
-                  {item.label}
-                </Link>
-              </li>
-            );
-          })}
-        </ol>
-      </div>
+      <p className={styles.description}>{description}</p>
+      <ol className={clsx(styles.list)}>
+        {items.map((item, index) => {
+          return (
+            <li key={index} className={clsx(styles.item)}>
+              {index !== 0 && (
+                <span className={styles.separator} aria-hidden="true">
+                  {separator}
+                </span>
+              )}
+              <Link
+                to={item.url}
+                className={clsx(styles.link)}
+                aria-current={index === items.length - 1 ? "page" : null}
+              >
+                {item.label}
+              </Link>
+            </li>
+          );
+        })}
+      </ol>
     </nav>
   );
 }
