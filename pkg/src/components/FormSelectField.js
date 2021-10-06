9cf76692fcf9aea291bb90dbac5fe8bd701929e4
Fredrik Johansson <jeanfredrik@gmail.com>
2021-10-06 18:45:46 +0200

    refactor: get form select icon from icon context

--- pkg/src/components/FormSelectField.js
+++ pkg/src/components/FormSelectField.js
@@ -1,3 +1,5 @@
+/** @jsx jsx */
+import { css, jsx } from "@emotion/react";
 import clsx from "clsx";
 import { Field } from "formik";
 import PropTypes from "prop-types";
@@ -7,27 +9,43 @@ import FormFieldDescription from "./FormFieldDescription";
 import FormFieldError from "./FormFieldError";
 import FormFieldLabel from "./FormFieldLabel";
 import FormFieldWrapper from "./FormFieldWrapper";
+import { useIconContext } from "./IconProvider";
+
 import * as defaultStyles from "./FormSelectField.module.css";
 
 FormSelectField.propTypes = {
   className: PropTypes.string,
   options: PropTypes.arrayOf(PropTypes.string),
   styles: PropTypes.objectOf(PropTypes.string),
+  icon: PropTypes.oneOfType([
+    PropTypes.exact({
+      name: PropTypes.string.isRequired,
+    }),
+    PropTypes.exact({
+      src: PropTypes.string.isRequired,
+    }),
+  ]),
 };
 
 export default function FormSelectField({
   className,
   options,
   styles = defaultStyles,
+
+  icon = { name: "chevron-down" },
   ...restProps
 }) {
+  const { getIconSrc } = useIconContext();
   return (
     <FormFieldWrapper
       className={clsx(styles.component, className)}
+      css={css({
+        "--form-select-icon": `url(${icon.src || getIconSrc(icon.name)})`,
+      })}
       {...restProps}
     >
       {({ name, controlProps }) => (
-        <>
+        <React.Fragment>
           <FormFieldLabel className={styles.label} />
           <FormFieldDescription className={styles.description} />
           <FormFieldError className={styles.error} />
@@ -50,7 +68,7 @@ export default function FormSelectField({
                 : null}
             </Field>
           </div>
-        </>
+        </React.Fragment>
       )}
     </FormFieldWrapper>
   );
