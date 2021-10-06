9f3fca52d7f4e0eb4bedc57e3f4dda1fc06dd73d
Fredrik Johansson <jeanfredrik@gmail.com>
2021-10-06 13:22:11 +0200

    feat: add `useComputedStyle` hook

--- pkg/src/hooks/index.js
+++ pkg/src/hooks/index.js
@@ -1,4 +1,5 @@
 export { default as useComponentWidth } from "./useComponentWidth";
+export { default as useComputedStyle } from "./useComputedStyle";
 export { default as useID } from "./useID";
 export { default as useSet } from "./useSet";
 export { default as useTheme } from "./useTheme";
