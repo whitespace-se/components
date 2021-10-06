793d087e2cb88c84b0e2d3df8aed8094dd58656c
Fredrik Johansson <jeanfredrik@gmail.com>
2021-10-06 18:12:58 +0200

    refactor: get form checkbox icon from icon context

--- pkg/src/components/FormCheckboxField.js
+++ pkg/src/components/FormCheckboxField.js
@@ -1,29 +1,44 @@
+/** @jsx jsx */
+import { css, jsx } from "@emotion/react";
 import clsx from "clsx";
 import { Field } from "formik";
 import PropTypes from "prop-types";
-import React from "react";
 
 import * as defaultStyles from "./FormCheckboxField.module.css";
 import FormFieldDescription from "./FormFieldDescription";
 import FormFieldError from "./FormFieldError";
 import FormFieldLabel from "./FormFieldLabel";
 import FormFieldWrapper from "./FormFieldWrapper";
+import { useIconContext } from "./IconProvider";
 
 FormCheckboxField.propTypes = {
   className: PropTypes.string,
   options: PropTypes.arrayOf(PropTypes.string),
   styles: PropTypes.objectOf(PropTypes.string),
+  icon: PropTypes.oneOfType([
+    PropTypes.exact({
+      name: PropTypes.string.isRequired,
+    }),
+    PropTypes.exact({
+      src: PropTypes.string.isRequired,
+    }),
+  ]),
 };
 
 export default function FormCheckboxField({
   className,
   options,
   styles = defaultStyles,
+  icon = { name: "checkmark" },
   ...restProps
 }) {
+  const { getIconSrc } = useIconContext();
   return (
     <FormFieldWrapper
       className={clsx(styles.component, className)}
+      css={css({
+        "--form-checkbox-image": `url(${icon.src || getIconSrc(icon.name)})`,
+      })}
       {...restProps}
     >
       {({ controlProps, name }) => (
