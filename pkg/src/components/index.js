a6001a4ce5ae07b3cd0ee7b841ac2a065687d583
Fredrik Johansson <jeanfredrik@gmail.com>
2021-10-22 10:09:42 +0200

    refactor: clean up exports

--- pkg/src/components/index.js
+++ pkg/src/components/index.js
@@ -1,47 +1,22 @@
 export { default as Accordion } from "./Accordion";
-export * from "./Accordion";
-
 export { default as Breadcrumbs } from "./Breadcrumbs";
-export * from "./Breadcrumbs";
-
 export { default as Button } from "./Button";
-export * from "./Button";
-
+export { default as CoverLink } from "./CoverLink";
 export { default as DashboardMenu } from "./DashboardMenu";
-export * from "./DashboardMenu";
-
 export { default as FormCheckboxField } from "./FormCheckboxField";
 export { default as FormInputField } from "./FormInputField";
 export { default as FormRadioGroupField } from "./FormRadioGroupField";
 export { default as FormSelectField } from "./FormSelectField";
 export { default as FormTextField } from "./FormTextField";
-
-export { default as Icon } from "./Icon";
-export * from "./Icon";
-
-export { default as IconProvider } from "./IconProvider";
-export * from "./IconProvider";
-
-export { default as CoverLink } from "./CoverLink";
-export * from "./CoverLink";
-
-export { default as Link } from "./Link";
-export * from "./Link";
-
-export { default as Pagination } from "./Pagination";
-export * from "./Pagination";
-
-export { default as RoundIcon } from "./RoundIcon";
-export * from "./RoundIcon";
-
 export { default as GridMenu } from "./GridMenu";
+export { default as Icon } from "./Icon";
+export { default as IconProvider } from "./IconProvider";
+export { default as Link } from "./Link";
+export { default as Pagination } from "./Pagination";
+export { default as RoundIcon } from "./RoundIcon";
 export { default as SkipTo } from "./SkipTo";
-
 export { default as ThemeProvider } from "./ThemeProvider";
-export * from "./ThemeProvider";
-
 export { default as TreeMenu } from "./TreeMenu";
-export * from "./TreeMenu";
-
 export { default as URLTransformerProvider } from "./URLTransformerProvider";
-export * from "./URLTransformerProvider";
+
+export * from "./IconProvider";
