d21c407f70bd706864378b415cca102a93819ccb
Fredrik Johansson <jeanfredrik@gmail.com>
2021-10-22 09:38:41 +0200

    refactor!: prop types for HeaderFrontPageMenu

--- pkg/src/components/HeaderFrontPageMenu.js
+++ pkg/src/components/HeaderFrontPageMenu.js
@@ -8,40 +8,38 @@ import RoundIcon from "./RoundIcon";
 import * as defaultStyles from "./HeaderFrontPageMenu.module.css";
 
 HeaderFrontPageMenu.propTypes = {
-  components: PropTypes.objectOf(PropTypes.elementType),
-  styles: PropTypes.objectOf(PropTypes.string),
+  as: PropTypes.elementType,
   className: PropTypes.string,
+  components: PropTypes.objectOf(PropTypes.elementType),
   items: PropTypes.arrayOf(
     PropTypes.shape({
-      url: PropTypes.string,
-      label: PropTypes.string,
-      description: PropTypes.string,
-      target: PropTypes.string,
+      label: PropTypes.node.isRequired,
+      description: PropTypes.node,
       icon: PropTypes.object,
     }),
   ).isRequired,
-  as: PropTypes.elementType,
+  styles: PropTypes.objectOf(PropTypes.string),
 };
 
 export default function HeaderFrontPageMenu({
+  as: Component = "nav",
+  className,
   components: { Link = HeaderFrontPageMenuLink, Icon = RoundIcon } = {
     Link: HeaderFrontPageMenuLink,
     Icon: RoundIcon,
   },
-  className,
   items,
   styles = defaultStyles,
-  as: Component = "nav",
   ...restProps
 }) {
   return (
     <Component className={clsx(styles.component, className)} {...restProps}>
       <ul className={clsx(styles.list)}>
         {items.map((item, index) => {
-          const { url, target, icon, label, description } = item;
+          const { icon, label, description, ...itemProps } = item;
           return (
             <li key={index} className={clsx(styles.item)}>
-              <Link className={clsx(styles.link)} to={url} target={target}>
+              <Link className={clsx(styles.link)} {...itemProps}>
                 {icon && <Icon className={clsx(styles.icon)} {...item.icon} />}
                 <div className={clsx(styles.text)}>
                   <p className={clsx(styles.title)}>{label}</p>
