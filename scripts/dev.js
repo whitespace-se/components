67aa4f521d75fd0637466ffe575eb8f9332c3732
Fredrik Johansson <jeanfredrik@gmail.com>
2021-11-22 10:45:24 +0100

    feat: improve dev script

--- scripts/dev.js
+++ scripts/dev.js
@@ -1,33 +1,49 @@
 import { build } from "esbuild";
+import { watch } from "chokidar";
 import { promisify } from "util";
-import { readFile } from "fs";
+import { readFileSync } from "fs";
 import glob from "fast-glob";
 import { resolve } from "path";
 import rimraf from "rimraf";
 
 const rimrafAsync = promisify(rimraf);
-const readFileAsync = promisify(readFile);
 
-async function run() {
-  let packageJSON = await readFileAsync(resolve("./package.json"));
-  const { main: outdir, source, esbuild = {} } = JSON.parse(packageJSON);
-  const entry = `${source}/**/*.{js,css}`;
-  await rimrafAsync(outdir);
-  const entryPoints = await glob(entry);
-  await build({
-    entryPoints,
-    outdir,
-    bundle: false,
-    loader: { ".js": "jsx" },
-    ...esbuild,
-    watch: {
-      onRebuild(error, result) {
-        void result;
-        if (error) console.error("watch build failed:", error);
-        else console.log("watch build succeeded");
-      },
-    },
-  });
-}
+let packageJSON = readFileSync(resolve("./package.json"));
+const { main: outdir, source, esbuild = {} } = JSON.parse(packageJSON);
+const entry = `${source}/**/*.{js,css}`;
 
-run().catch(() => process.exit(1));
+/**
+ * Builds the code in no time
+ */
+const run = async () => {
+  //Start build service
+  try {
+    await rimrafAsync(outdir);
+    const entryPoints = await glob(entry);
+
+    // // Get time before build starts
+    const timerStart = Date.now();
+
+    // Build code
+    await build({
+      entryPoints,
+      outdir,
+      bundle: false,
+      loader: { ".js": "jsx" },
+      ...esbuild,
+    });
+
+    // Get time after build ends
+    const timerEnd = Date.now();
+    console.log(`Built in ${timerEnd - timerStart}ms.`);
+  } catch (e) {
+    // OOPS! ERROR!
+  }
+};
+
+const watcher = watch([entry]);
+run();
+console.log(`Watching ${entry}...`);
+watcher.on("all", () => {
+  run();
+});
