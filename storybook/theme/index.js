d3311d318741abbed260bb26cc54939bdf32e8a5
Peter Antonius <peter.antonius@whitespace.se>
2021-12-22 08:56:44 +0100

    fix: add props support and add missing props

--- storybook/theme/index.js
+++ storybook/theme/index.js
@@ -5,6 +5,7 @@ import "./Card.props.css";
 import "./DashboardMenu.props.css";
 import "./Form.props.css";
 import "./GridMenu.props.css";
+import "./Link.props.css";
 import "./List.props.css";
 import "./PageGrid.props.css";
 import "./SkipTo.props.css";
